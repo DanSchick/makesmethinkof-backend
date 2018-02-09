fc8dbe33ae876cacaa403260461412ae07c59586
DanSchick <schickd12@gmail.com>
2018-02-09 14:27:58 -0500

    added postgres pool

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var app = express();
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const port = process.env.PORT || 5000;
 
-const client = new Client({
+const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true
 });
-client.connect();
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
 app.get('/api/relation/:id', (req, res) => {
-    client.query('SELECT * FROM things;', (err, response) => {
+    pool.query('SELECT * FROM things;', (err, response) => {
         if(err) throw err;
         res.status(200).json(response);
     });
 });
 
+// app.put('/api/relation/:thing1/:thing2', (req, res) => {
+//     pool.query('INSERT INTO things (id, data) VALUES ( '1', 'imdb11111' )', (err, response) => {
+//         if(err) throw err;
+//     });
+// });
 
 app.listen(port, function() {
     console.log('listening on ' + port);
