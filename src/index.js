d5724ff31fe5429a2fb327d02a0cfc73f30f2978
DanSchick <schickd12@gmail.com>
2018-02-09 14:36:02 -0500

    added an insert POST route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var app = express();
 const { Pool } = require('pg');
 const port = process.env.PORT || 5000;
 
+// set up pool
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true
@@ -12,6 +13,9 @@ pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err)
     process.exit(-1)
 });
+
+// set up to consume JSONin request
+app.use(express.bodyParser());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
@@ -24,11 +28,13 @@ app.get('/api/relation/:id', (req, res) => {
     });
 });
 
-// app.put('/api/relation/:thing1/:thing2', (req, res) => {
-//     pool.query('INSERT INTO things (id, data) VALUES ( '1', 'imdb11111' )', (err, response) => {
-//         if(err) throw err;
-//     });
-// });
+app.put('/api/relation/insert', (req, res) => {
+    pool.query("SELECT * FROM things", (err, response) => {
+        console.log(req.body);
+        if(err) throw err;
+        res.status(200).json(req.body);
+    });
+});
 
 app.listen(port, function() {
     console.log('listening on ' + port);
