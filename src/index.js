5214b80faed2b68830e2c8f500bae6aa3eb17127
DanSchick <schickd12@gmail.com>
2018-02-10 16:15:59 -0500

    added CORS
    - error after deploy, need CORS to be able to access API.
    - Told express to allow CORS

--- src/index.js
+++ src/index.js
@@ -25,6 +25,13 @@ pool.on('error', (err, client) => {
 
 // set up to consume JSON request from body
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
